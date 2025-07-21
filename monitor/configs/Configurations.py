91d0caf14531a14716d10457834555ee5579dc63
haris rehman <harisrehmanchugtai@gmail.com>
2025-07-21 19:58:30 +0500

    further modularized the code and added separate  configs. Removed local hash storage and added new requirements

---
+++ monitor/configs/Configurations.py
@@ -0,0 +1,9 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+MONGO_URI = os.getenv("MONGO_URI")
+SENDER_EMAIL = os.getenv("SENDER_EMAIL")
+SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
+CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL_SECONDS", 30)) #30 minutes
