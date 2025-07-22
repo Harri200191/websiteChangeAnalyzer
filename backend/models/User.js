995ccd194354ca652153ccedc134a9c38e50b173
haris rehman <harisrehmanchugtai@gmail.com>
2025-07-22 14:39:01 +0500

    fixed backend dockerfile and updated code with bycryptjs instead

--- backend/models/User.js
+++ backend/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
+const bcryptjs = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: true },
@@ -26,12 +26,12 @@ const userSchema = new mongoose.Schema({
 
 userSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 10);
+  this.password = await bcryptjs.hash(this.password, 10);
   next();
 });
 
 userSchema.methods.comparePassword = function(candidatePassword) {
-  return bcrypt.compare(candidatePassword, this.password);
+  return bcryptjs.compare(candidatePassword, this.password);
 };
 
 module.exports = mongoose.model('users', userSchema); 
