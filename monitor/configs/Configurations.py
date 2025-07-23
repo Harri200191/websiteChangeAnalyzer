893e3f02067074fa6e0900630e433344f5afd35b
Harri200191 <harisrehmanchugtai@gmail.com>
2025-07-23 14:09:53 +0500

    took frontend tag from env

--- monitor/configs/Configurations.py
+++ monitor/configs/Configurations.py
@@ -6,4 +6,5 @@ load_dotenv()
 MONGO_URI = os.getenv("MONGO_URI")
 SENDER_EMAIL = os.getenv("SENDER_EMAIL")
 SENDER_PASSWORD = os.getenv("SENDER_PASSWORD")
-CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL_SECONDS", 30)) #30 minutes
+CHECK_INTERVAL = int(os.getenv("CHECK_INTERVAL_SECONDS", 1800)) 
+FRONTEND_TAG = os.getenv("FRONTEND_TAG")
